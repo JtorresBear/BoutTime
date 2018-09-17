f3064f9aa621f7d26032f36fc852015c5a9901df
Juan Torres <jmmtorres1991@yahoo.com>
2018-09-17 16:28:49 -0500

    added the starter project

---
+++ Bout Time/BoutTime1/BoutTime1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BoutTime1
+//
+//  Created by Juan Torres on 9/17/18.
+//  Copyright © 2018 Juan Torres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
