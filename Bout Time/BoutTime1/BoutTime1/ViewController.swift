3773d9cbdf2ec4994b093993c60ca50030de3d06
Juan Torres <jmmtorres1991@yahoo.com>
2018-09-19 06:50:03 -0500

    added comments. changed events and values

--- Bout Time/BoutTime1/BoutTime1/ViewController.swift
+++ Bout Time/BoutTime1/BoutTime1/ViewController.swift
@@ -7,12 +7,13 @@
 //
 
 import UIKit
+import AVFoundation
 
 var score = " " 
 
 
 class ViewController: UIViewController {
-    
+    // Just a bunch of IBOutlets!!
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var nextRoundButton: UIButton!
     @IBOutlet weak var firstEvent: UILabel!
@@ -29,13 +30,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var yourScoreLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var playAgainButton: UIButton!
+    @IBOutlet weak var shakeLabel: UILabel!
+    
+    //creating for audio purposes. don't forget to import AVFoundation
+    var player: AVAudioPlayer?
     
     
     
-    
-    
-    
-    
+    //Setting up some variables for the ViewController to keep
     var time = 60
     var timer = Timer()
     var game = Game()
@@ -43,7 +45,8 @@ class ViewController: UIViewController {
     
     
    
-    
+    //this function makes the timer go down and displays it.
+    //it also stops the game if the counter reaches 0.
     @objc func countDownTimer()
     {
         time -= 1
@@ -54,14 +57,13 @@ class ViewController: UIViewController {
         }
         
     }
-    
-
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         timeLabel.text = "60"
-       displayEvents()
+        displayEvents()
+        //Starts the timer when the game starts
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.countDownTimer), userInfo: nil, repeats: true)
         nextRoundButton.isHidden = true
         gameOverButton.isHidden = true
@@ -75,7 +77,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    
+    //Displays Events using hardCoded indexes because they will always hold the same place unless changed.
     func displayEvents()
     {
         firstEvent.text = game.gameEvents.listOfEvents[game.randomArrayForRound[0]].event
@@ -84,6 +86,9 @@ class ViewController: UIViewController {
         lastEvent.text = game.gameEvents.listOfEvents[game.randomArrayForRound[3]].event
     }
     
+    
+    //Switches the labels to read differently. although this function doesn't change the display
+    //the function that changes the display is called inside this function
     @IBAction func changeEvents(_ sender: UIButton) {
         
         switch sender
@@ -108,14 +113,17 @@ class ViewController: UIViewController {
     
     
     
-    
+    //this function changes the values of the indexes using its because they are given ints
+    //the ints given are for the events array in the game. and the other indexes are for
+    // the random array to hold the new values.
+    //then the displayevents fuction is called to repoppulate the labels
     func switchFirst(_ label: Int, with secondLabel: Int, at index: Int, _ withIndex: Int)
     {
         game.randomArrayForRound[index] = secondLabel
         game.randomArrayForRound[withIndex] = label
         displayEvents()
     }
-    
+    //just initiates a new round. resets values and calls functions that need to be called
     func newRound()
     {
         game.newRound()
@@ -126,22 +134,23 @@ class ViewController: UIViewController {
     }
     
     
-    
+    // checks if the device has been shaken, and then ends the round by calling the endRound() function
     override func motionBegan(_ motion: UIEventSubtype, with event: UIEvent?) {
         endRound()
-        
     }
     
 
+    //starts a a button that starts a new round and changes values
     @IBAction func newRound(_ sender: UIButton) {
         newRound()
         nextRoundButton.isEnabled = false
         nextRoundButton.isHidden = true
         enableButton(isTrue: true)
+        shakeLabel.isHidden = false
         
     }
     
-    
+    // disables or enables buttons so they can't switch values when the round is over
     func enableButton(isTrue: Bool)
     {
         firstDownButton.isEnabled = isTrue
@@ -152,9 +161,11 @@ class ViewController: UIViewController {
         thirdUpButton.isEnabled = isTrue
         
     }
-    
+    //ends the round. also calls on game functions to figure out if the round is correct or incorrect
+    //changes score sets the button images as needed. also plays a sound as needed.
     func endRound()
     {
+        shakeLabel.isHidden = true
         round += 1
         timer.invalidate()
         if(game.roundEnd())
@@ -162,12 +173,15 @@ class ViewController: UIViewController {
             nextRoundButton.setImage(#imageLiteral(resourceName: "next_round_success") ,for: .normal)
             nextRoundButton.isEnabled = true
             enableButton(isTrue: false)
+            playSound(if: true)
+            
         }
         else
         {
             nextRoundButton.setImage(#imageLiteral(resourceName: "next_round_fail"), for: .normal)
             nextRoundButton.isEnabled = true
             enableButton(isTrue: false)
+            playSound(if: false)
         }
         
         nextRoundButton.isHidden = false
@@ -182,6 +196,7 @@ class ViewController: UIViewController {
         score = "\(game.score)/6"
     }
     
+    //hides or shows labels and buttons
     func showScoreAndButtons(isHidden: Bool)
     {
         yourScoreLabel.isHidden = isHidden
@@ -201,13 +216,14 @@ class ViewController: UIViewController {
         thirdUpButton.isHidden = !isHidden
     }
     
+    //shows the game is over. then proceeds to end the game and show what looks like a new screen
     @IBAction func gameOver(_ sender: UIButton) {
         scoreLabel.text = "\(game.score)/6"
         
         showScoreAndButtons(isHidden: false)
         gameOverButton.isHidden = true
     }
-    
+    //starts a new game and resets everything that needs to be.
     @IBAction func newGame(_ sender: UIButton) {
         round = 0
         showScoreAndButtons(isHidden: true)
@@ -215,8 +231,48 @@ class ViewController: UIViewController {
         nextRoundButton.isHidden = true
         enableButton(isTrue: true)
         game.score = 0
+        shakeLabel.isHidden = false
     }
     
+    //plays a sound. the sound is loaded depending on whether the round is correct or not. 
+    func playSound(if CorrectOrFalse: Bool)
+    {
+        var mainUrl: URL?
+        
+        if(!CorrectOrFalse)
+        {
+            guard let url = Bundle.main.url(forResource: "IncorrectBuzz", withExtension: "wav")
+                else
+                {
+                    print("url not found")
+                    return
+                }
+            mainUrl = url
+        }
+        else
+        {
+            guard let url = Bundle.main.url(forResource: "CorrectDing", withExtension: "wav")
+                else
+            {
+                print("url not found")
+                return
+            }
+            mainUrl = url
+        }
+            
+        do {
+            
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            player = try AVAudioPlayer(contentsOf: mainUrl!, fileTypeHint: AVFileType.mp3.rawValue)
+            
+            player!.play()
+        } catch let error as NSError {
+            print("error: \(error.localizedDescription)")
+        }
+            
+        }
     
     
 }
